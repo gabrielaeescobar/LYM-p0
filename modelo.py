4622110b127fc00ce835fbc64ab4f84526537a38
MariaaPinzon <ma.pinzonr1@uniandes.edu.co>
2023-08-30 18:58:21 -0500

    lower

--- modelo.py
+++ modelo.py
@@ -4,11 +4,12 @@ nltk.download('punkt')
 from nltk.tokenize import word_tokenize, sent_tokenize
 import cargar 
 
-texto = cargar.openFile2(input("ingrese el filename (sin el .txt): "))
+texto_carga = cargar.openFile2(input("ingrese el filename (sin el .txt): "))
+texto = texto_carga.lower()
 
-texto_prueba = " defVar nom 0 defVar x 0 defVar y 0 defVar one 0 defProc putCB (c, b) { drop(c); letGo (b) ; walk(n) }"
-texto_prueba2 = "defProc goNorth() { while can ( walk (1 , north )) { walk (1 , north ) }; putCB (1 ,1) } defProc hola (cara,de) "
-todo = sent_tokenize(texto_prueba2)
+#texto_prueba = " defVar nom 0 defVar x 0 defVar y 0 defVar one 0 defProc putCB (c, b) { drop(c); letGo (b) ; walk(n) }"
+#texto_prueba2 = "defProc goNorth() { while can ( walk (1 , north )) { walk (1 , north ) }; putCB (1 ,1) } defProc hola (cara,de) "
+todo = sent_tokenize(texto)
 tokens = [word_tokenize(cadacosa) for cadacosa in todo]
 #print("Tokens:", tokens)
 
@@ -32,7 +33,7 @@ def check_defVar(tokens_):
     tokens = tokens_[0]
     while i < len(tokens):
         token = tokens[i]
-        if token == "defVar":
+        if token == "defvar":
             hubo = "si_hubo"
             nombre = tokens[i+1]
             valor = tokens[i+2]
@@ -56,7 +57,7 @@ def check_defProc(tokens_):
     hubo = "no_hubo"
     while i<len(tokens):
         token = tokens[i]
-        if token == "defProc":
+        if token == "defproc":
             hubo = "si_hubo"
             nombre = tokens[i+1]
             lista_nombres.append(nombre)
@@ -82,7 +83,7 @@ def check_defProc(tokens_):
                     if token"""
                 
         
-print(check_defProc(tokens), "defProc")
+print(check_defProc(tokens), "defproc")
 
 
 """""def check_turnto(comando):
