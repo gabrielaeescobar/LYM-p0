4e27cc23a3e9e10a7481066dc5a755b61d7d05ae
MariaaPinzon <ma.pinzonr1@uniandes.edu.co>
2023-08-31 15:15:00 -0500

    merge branch maria funcion block

--- modelo.py
+++ modelo.py
@@ -6,9 +6,9 @@ nltk.download('punkt')
 texto_carga = cargar.openFile2(input("ingrese el filename (sin el .txt): "))
 texto = texto_carga.lower()
 
-#texto_prueba = " defVar nom 0 defVar x 0 defVar y 0 defVar one 0 defProc putCB (c, b) { drop(c); letGo (b) ; walk(n) }"
+texto_prueba = "defvar n 0 defproc goNorth() { while can ( walk (1 , north )) { walk (1, 3 , 4 , north ) }; putCB (1 ,1) } defproc hola (cara,de) { goNorth() }"
 #texto_prueba2 = "defProc goNorth() { while can ( walk (1 , north )) { walk (1 , north ) }; putCB (1 ,1) } defProc hola (cara,de) "
-todo = sent_tokenize(texto)
+todo = sent_tokenize(texto_prueba)
 tokens = [word_tokenize(cadacosa) for cadacosa in todo]
 #print("Tokens:", tokens)
 
@@ -25,12 +25,14 @@ def check_defVar(tokens_):
     i = 0
     check = False
     existe = False
+    lista_nombres = []
     tokens = tokens_[0]
     while i < len(tokens):
         token = tokens[i]
         if token == "defvar":
             existe = True
             nombre = tokens[i+1]
+            lista_nombres.append(nombre)
             valor = tokens[i+2]
             
             if isinstance(nombre, str) and valor.isdigit():
@@ -39,13 +41,13 @@ def check_defVar(tokens_):
 
         i += 1
     
-    return check,existe
+    return check,existe,lista_nombres
 
 #Funcion para defProc
 def check_defProc(tokens_):
     i= 0
     tokens = tokens_[0]
-    lista_nombres = []
+    dict_nombres = {}
     check = False
     existe = False
     while i<len(tokens):
@@ -53,21 +55,25 @@ def check_defProc(tokens_):
         if token == "defproc":
             existe = True
             nombre = tokens[i+1]
-            lista_nombres.append(nombre)
+            dict_nombres[nombre] = ""
             parentesis_izquierdo = tokens[i+2]
             if isinstance(nombre,str) and parentesis_izquierdo == "(":
                 continuacion = tokens[i+3]
                 if continuacion == ")": # primer caso 0 parametros
                     check = True
+                    dict_nombres[nombre] = [tokens[i+2],continuacion]
                 elif isinstance(continuacion,str) and tokens[i+4] == ")": # segundo caso 1 parametro
                     check = True
+                    dict_nombres[nombre] = [tokens[i+2],continuacion,tokens[i+4]]
                 elif isinstance(continuacion,str) and tokens[i+4] == "," and isinstance(tokens[i+5],str) and tokens[i+6] == ")": # tercer caso 2 parametros
                     check = True
+                    dict_nombres[nombre] = [tokens[i+2],continuacion,tokens[i+4],tokens[i+5],tokens[i+5]]
                 else:
                     check = False
 
         i += 1
-    return check,existe,lista_nombres    
+    return check,existe,dict_nombres  
+
 
 #COMANDOS
 def Walk_Leap (lista_variables_creadas, Directions, Orientations, num, tokens_):
@@ -174,3 +180,60 @@ def Nop(tokens_):
                 check = False
         i += 1
     return check, existe
+        
+
+def blockCommands(dict_nombres_proc,lista_variables_creadas, Directions, Orientations, num, tokens_):
+    i = 0 
+    check = False
+    existe = False
+    values_dict_nombres_proc = dict_nombres_proc.values()
+    keys_dict_nombres_proc = dict_nombres_proc.keys()
+    print(keys_dict_nombres_proc,"what")
+
+    tokens = tokens_[0]
+    while i < len(tokens):
+        if tokens[i] == "{":
+            existe = True
+            if isinstance(tokens[i+1],str):
+
+                if Walk_Leap (lista_variables_creadas, Directions, Orientations, num, tokens_) == (True, True) :
+                    check = True 
+                elif Jump (lista_variables_creadas, num, tokens_) == (True, True) :
+                    check = True 
+                elif Turn (Directions, tokens_)  == (True, True) :
+                    check = True 
+                elif TurnTo (Orientations, tokens_) == (True, True) :
+                    check = True 
+                elif Drop_Get_Grab_LetGo (lista_variables_creadas, num, tokens_) == (True, True) :
+                    check = True 
+                elif Nop(tokens_) == (True, True) :
+                    check = True
+                elif tokens[i+1] in keys_dict_nombres_proc:
+                    #if tokens[i+1] == dict_nombres_proc[tokens[i+1]]: 
+                    check = True                  
+                else:
+                    check = False
+        i+=1
+    return check,existe
+
+list_variables_names_tupla = check_defVar(tokens)
+list_variables_names = list_variables_names_tupla[2]
+
+print(list_variables_names,"lol")
+
+dict_nombres_proc_tupla = check_defProc(tokens)
+dict_nombres_proc = dict_nombres_proc_tupla[2]
+
+print(dict_nombres_proc,"loool")
+
+
+print(blockCommands(dict_nombres_proc,list_variables_names,Directions,Orientations,num,tokens ), "ojala funcione")
+
+
+# falta implementar esta funcion en el defProc
+
+# recorro la lista de cada nombre y tiene que ser igual a la estructura que es correcta
+
+# CAMBIAR EL IS INSTANCE POR UNA FUNCION AUXILIAR QUE RECORRA CADA CADENA DEL NOMBRE
+
+# mirar que los metodos que se implementan abajo tienen que tener las mismas variables que la definicion de proc ¿puede entrar cualquier cosa?
